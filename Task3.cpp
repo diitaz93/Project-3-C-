7ae44b4e21ef9fdbf05b90d4271c9122aa7b2251
diitaz93 <juansdb93@gmail.com>
2019-11-24 18:29:54 +0100

    Task 3 compiling but grid generation is wrong

--- Task3.cpp
+++ Task3.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include "Project3.hpp"
 #include <cstdio>
+
+
 class Domain
 {
   Curvebase *sides[4];
-  double *x,*y;
-  int m,n;
+  double *x,*y; //Array of coordinates of each inner node
+  int m,n; //Number of internal nodes
 public:
-  Domain(Curvebase&,Curvebase&,Curvebase&,Curvebase&);
-  Domain(const Domain&);
-  Domain(Domain&&);
-  Domain& operator=(Domain&);
-  ~Domain();
+  //Domain(LeftCurve&, RightCurve&, LowerCurve&, UpperCurve&);
+  Domain(Curvebase&,Curvebase&,Curvebase&,Curvebase&); //Constructor
+  //Domain(const Domain&); //Copy constructor
+  //Domain(Domain&&); // Move constructor
+  Domain& operator=(const Domain&); //Copy assignment
+  ~Domain(); // Destructor
   void generate_grid(int,int);//number of internal nodes in each direction
-  void Output();
+  void Output(); // Writting internal nodes to file
+  int indicator()
+  {
+    return m;
+  }//Test function (temporary)
 };
+
+// Constructor
 Domain::Domain(Curvebase& s1,Curvebase& s2,Curvebase& s3,Curvebase& s4)
+//Domain::Domain(LeftCurve& s1, RightCurve& s2, LowerCurve& s3, UpperCurve& s4)
 {
   sides[0]=&s1;//Left
   sides[1]=&s2;//Right
   sides[2]=&s3;//Lower
-  sides[3]=&s4;
+  sides[3]=&s4;//Upper
   m=n=0;
   x=y=nullptr;
 }
+//Copy constructor
+/*
 Domain::Domain(const Domain& d):m(d.m),n(d.n),x(nullptr),y(nullptr)
 {
   if(m>0)
@@ -37,6 +49,9 @@ Domain::Domain(const Domain& d):m(d.m),n(d.n),x(nullptr),y(nullptr)
 	}
     }
 }
+*/
+// Move constructor
+/*
 Domain::Domain(Domain&& d) noexcept:m(d.m),n(d.n),x(d.x),y(d.y)
 {
   d.m=0;
@@ -44,7 +59,9 @@ Domain::Domain(Domain&& d) noexcept:m(d.m),n(d.n),x(d.x),y(d.y)
   d.x=nullptr;
   d.y=nullptr;
 }
-Domain& Domain::operator=(const DOmain& d)
+*/
+// Copy assignment
+Domain& Domain::operator=(const Domain& d)
 {
   if(this!=&d)
     {//dont copy to itself
@@ -75,6 +92,7 @@ Domain& Domain::operator=(const DOmain& d)
 	}
     }
 }
+// Destructor
 Domain::~Domain()
 {
   if(m>0)
@@ -83,12 +101,12 @@ Domain::~Domain()
       delete [] y;
     }
 }
-Domain::grid_generation(int m_,int n_)
+void Domain::generate_grid(int m_,int n_)
 {
   //m on x;n on y
   if(m_<=0 || n_<=0)
     {
-      cerr<<"meaningless number!";
+      std::cerr<<"meaningless number!";
       exit(EXIT_FAILURE);
     }
   else
@@ -101,21 +119,23 @@ Domain::grid_generation(int m_,int n_)
       m=m_;n=n_;
       x=new double[m*n];
       y=new double[m*n];
-      double Corner_x={-10.0,5.0,-10.0,5.0};
+      double ksi,eta;
+      double Corner_x[4]={-10.0,5.0,-10.0,5.0};
       //Specialized here, only works in this problem
       double Corner_y[4]={0.0,0.0,3.0,3.0};
+      int k;
       for(int i=0;i<n;i++)//x direction 
 	for(int j=0;j<m;j++)//y direction
 	  {
 	    ksi=i/m;
 	    eta=j/n;
 	    k=i*m+j;//Lexicograghical rule
-	    x[k]=ksi * sides[0].x(eta) + (1-ksi) * sides[1].x(eta)
-	      + eta * sides[2].x(ksi) + (1-eta) * sides[3].x(ksi)
+	    x[k]=ksi * (*sides[0]).x(eta) + (1-ksi) * (*sides[1]).x(eta)
+	      + eta * (*sides[2]).x(ksi) + (1-eta) * (*sides[3]).x(ksi)
 	      - ksi*eta * Corner_x[0] - (1-ksi)*eta * Corner_x[1]
 	      - ksi*(1-eta) * Corner_x[2] - (1-ksi)*(1-eta) * Corner_x[4];
-	    y[k]=ksi * sides[0].y(eta) + (1-ksi) * sides[1].y(eta)
-	      + eta * sides[2].y(ksi) + (1-eta) * sides[3].y(ksi)
+	    y[k]=ksi * (*sides[0]).y(eta) + (1-ksi) * (*sides[1]).y(eta)
+	      + eta * (*sides[2]).y(ksi) + (1-eta) * (*sides[3]).y(ksi)
 	      - ksi*eta * Corner_y[0] - (1-ksi)*eta * Corner_y[1]
 	      - ksi*(1-eta) * Corner_y[2] - (1-ksi)*(1-eta) * Corner_y[4];
 	  }
--- test.cpp
+++ test.cpp
@@ -1,11 +1,20 @@
 #include "Project3.hpp"
+#include "Task1.cpp"
 #include "Task2.cpp"
+#include "Task3.cpp"
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 int main()
 {
-  LowerCurve L1(-10.0,5.0);
-  std::cout<<L1.x(0.5)<<std::endl;
+  LeftCurve L1(0.0,3.0);
+  RightCurve L2(0.0,3.0);
+  LowerCurve L3(-10.0,5.0);
+  UpperCurve L4(-10.0,5.0);
+  std::cout<<L3.x(0.5)<<std::endl;
+  Domain D(L1,L2,L3,L4);
+  D.generate_grid(15,3);
+  std::cout<<D.indicator()<<"\n";
   return 1;
 }
